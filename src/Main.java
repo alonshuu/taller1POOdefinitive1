6d020bf42da54a2b027f9a4375020a7a571e7a2d
Alonso Lagos <a.lagos10@ufromail.cl>
2024-09-06 10:34:36 -0400

    ultimo commit

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,19 @@ public class Main {
 
     public static void main(String[] args) {
         menu();
+        opciones();
 
 
     }
 
     public static void menu() {
-        System.out.println("--- MENU (MATRIZ CREADA CON ÉXITO) ---");
+        System.out.println("--- MENU (MATRIX CREATION YEAH) ---");
         System.out.println("A) Para crear una nueva matriz");
-        System.out.println("B) Para mostrar tu matriz por fila");
-        System.out.println("C) Para verificar si la matriz creada es de tipo CERO");
-        opciones();
+        System.out.println("B) Para llenar tu matriz");
+        System.out.println("C) Para mostrar tu matriz por fila");
+        System.out.println("D) Para verificar si la matriz creada es de tipo CERO");
+        System.out.println("Q) Para salir de la aplicacion");
+
 
 
     }
@@ -25,23 +28,36 @@ public class Main {
 
     static int crearFila() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Ingresa el número de filas");
-        int filas = in.nextInt();
-        in.nextLine();
-        return(filas);
+        int fila = -1;
+        do {
+            System.out.println("Ingresa el número de filas");
+            fila = in.nextInt();
+            if (fila <= 0) {
+                System.out.println("Por favor ingresa un numero valido");
+            }
+
+        } while (fila <= 0);
+
+        return(fila);
 
     }
 
     static int crearCols() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Ingresa el número de Columnas");
-        int cols = in.nextInt();
-        in.nextLine();
+        int fila = -1;
+        do {
+            System.out.println("Ingresa el número de columnas");
+            fila = in.nextInt();
+            if (fila <= 0) {
+                System.out.println("Por favor ingresa un numero valido");
 
-        return(cols);
+            }
+
+        } while (fila <= 0);
+
+        return(fila);
 
     }
-
 
 
     public static void llenarMatriz(int[][] matriz, int filas, int cols) {
@@ -51,6 +67,8 @@ public class Main {
                 matriz[i][j] = random.nextInt(10);
             }
         }
+        System.out.println("MATRIZ LLENADA CON EXITO");
+        menu();
     }
 
 
@@ -67,11 +85,24 @@ public class Main {
                     filas = crearFila();
                     cols = crearCols();
                     matris = crearMatriz(filas,cols);
-                    System.out.println(Arrays.deepToString(matris));
+
                     break;
                 case "B":
                     llenarMatriz(matris, filas, cols);
-                    System.out.println(Arrays.deepToString(matris));
+                    break;
+
+
+                case "C":
+                    mostrarMatriz(matris);
+                    break;
+
+                case "D":
+                    boolean a = matrizCero(matris);
+                    if a = true;
+
+
+
+
 
 
 
@@ -85,14 +116,39 @@ public class Main {
     }
 
 
-
-
-    boolean ValidarDimensiones() {
-
-        return(false);
+    public static void mostrarMatriz(int[][] matrix){
+        System.out.println(Arrays.deepToString(matrix));
     }
 
+
+    static boolean matrizCero (int matriz[][]) {
+        int contador = 0;
+        int largo = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; largo++) {
+                if (matriz[i][j] == 0) {
+                    contador++;
+
+                }
+            }
+
+        }
+
+        return contador > largo/2;
+
+    }
+
+
+
+
+
+
+
     static int [][] crearMatriz(int filas, int cols) {
+
+        System.out.println("MATRIZ CREADA CON EXITO");
+        menu();
 
         return(new int[filas][cols]);
     }
