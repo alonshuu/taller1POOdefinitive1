ed808bc962e552030b3287a83aafd2f90e458b20
Alonso Lagos <a.lagos10@ufromail.cl>
2024-09-06 09:36:26 -0400

    AVANZADO FUNCIONES COMO EL MENU, LLENARMATRIZ.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,115 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        run();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
+
+    public static void run(){  //metodo run para inicar el mensajeInicial, el cual espero dps inicie el Menu
+
+        llenarMatriz();
+
+
+
+    }
+
+
+    static int llenarFila() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingresa el número de filas");
+        int filas = in.nextInt();
+        in.nextLine();
+        return(filas);
+
+    }
+
+    static int llenarCols() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingresa el número de Columnas");
+        int cols = in.nextInt();
+        in.nextLine();
+        return(cols);
+
+    }
+
+
+
+
+
+    public static void llenarMatriz() {
+        System.out.println("Hola bienvenido al sistema de creación de matrices.");  // muestra el primer mensaje
+        Scanner in = new Scanner(System.in);   // considero mala practica pero por ahora todavia no hago el metodo aparte asi q ; _ ;
+        System.out.println("Ingresa el numero de filas");
+        int filas = llenarFila();
+        int cols = llenarCols();
+        int[][] matris = crearMatriz(filas,cols);
+        System.out.println(Arrays.deepToString(matris));
+
+
+
+
+
+
+
+
+    }
+
+
+
+    public static void menu() {
+
+        System.out.println("--- MENU (MATRIZ CREADA CON ÉXITO) ---");
+        System.out.println("A) Para crear una nueva matriz");
+        System.out.println("B) Para mostrar tu matriz por fila");
+        System.out.println("C) Para verificar si la matriz creada es de tipo CERO");
+
+
+
+
+    }
+
+    public static void elegirOpcion() {
+
+
+    }
+
+
+    boolean ValidarDimensiones() {
+
+
+
+
+        return(false);
+    }
+
+
+
+
+
+    static int [][] crearMatriz(int filas, int cols) {
+
+        return(new int[filas][cols]);
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
 }
